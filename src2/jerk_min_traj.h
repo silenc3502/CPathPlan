3c0bbc7f99a7325a160c99cc3cd1bfe19031ad0b
silenc3502 <gcccompil3r@gmail.com>
2018-10-20 16:39:56 +0900

    Try to some challenge

---
+++ src2/jerk_min_traj.h
@@ -0,0 +1,10 @@
+#ifndef __JERK_MIN_TRAJ__
+#define __JERK_MIN_TRAJ__
+
+#include <vector>
+
+using namespace std;
+
+vector<double> get_traj_coeffs(vector<double> start, vector<double> end, double T);
+
+#endif
